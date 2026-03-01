eea9b8ad01b5194a299c88a0b66ee7f004a4b8ac
Arvin Hasim <arvin.hasim@binus.ac.id>
2026-03-01 23:38:58 +0700

    #feat mobile breakpoint adjustment

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,21 +6,21 @@ export default function Main() {
   return (
     <div>
       {/* hero */}
-      <div className="flex flex-col items-center justify-center h-dvh gap-6 px-6">
+      <div className="flex flex-col items-center justify-center h-fit py-20 lg:h-dvh  gap-6 px-6">
         <motion.h1
-          className="text-6xl font-bold text-center"
+          className="text-4xl lg:text-6xl font-bold text-center"
           initial={{ opacity: 0, scale: 0 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{
             scale: { type: "spring", visualDuration: 0.5, bounce: 0.5 },
           }}
         >
-          The <span className="underline">first choice</span> for your
+          The <br className="lg:hidden"/> <span className="underline">first choice</span> for your
           <br />
           ticketing solution
         </motion.h1>
         <p>Book your first ticket now</p>
-        <div className="flex border rounded-lg divide-x mb-24">
+        <div className="flex border rounded-lg divide-x lg:mb-24 scale-70 origin-center lg:scale-100">
           <div className="flex flex-col items-center px-6 py-4">
             <Van size={24} strokeWidth={1.5} />
             <span className="text-sm text-center">Ground Transport Ticket</span>
@@ -39,7 +39,7 @@ export default function Main() {
           </div>
         </div>
         <motion.div
-          className="text-sm flex flex-col items-center text-gray-400"
+          className="text-sm lg:flex flex-col items-center text-gray-400 hidden"
           animate={{ y: [0, -8] }}
           transition={{
             type: "spring",
@@ -57,13 +57,13 @@ export default function Main() {
       </div>
 
       {/* about */}
-      <div className="relative flex flex-col items-center justify-center h-dvh gap-6 px-6">
+      <div className="relative flex flex-col items-center justify-center h-fit mb-20 lg:h-dvh gap-6 px-6">
         <motion.div
           initial={{ opacity: 0, x: -100 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.8, ease: "easeOut" }}
           viewport={{ once: true }}
-          className="absolute left-6 top-7"
+          className="absolute left-6 top-7 hidden lg:block"
         >
           <Image
             src="/passport_prop.png"
@@ -73,7 +73,7 @@ export default function Main() {
           />
         </motion.div>
 
-        <p className="max-w-lg text-center text-xl">
+        <p className="max-w-lg text-center text-lg lg:text-xl ">
           Acceloka is where every ticket finds its place. Cinema, concerts,
           ground transport, sea voyages, flights, and hotels — all in one
           refined booking experience.
@@ -82,7 +82,7 @@ export default function Main() {
           View available tickets
         </button>
         <motion.div
-          className="absolute right-6 bottom-7"
+          className="absolute right-6 bottom-7 hidden lg:block"
           initial={{opacity: 0, x: 100}}
           whileInView={{opacity: 1, x: 0}}
           transition={{duration: 0.4, ease: "easeOut"}}
